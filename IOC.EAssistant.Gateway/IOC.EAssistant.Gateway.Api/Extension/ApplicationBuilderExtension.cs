e279e45d2fd33c0c49796f319646ac3d0378106b
onededios <joel10olor@gmail.com>
2025-11-17 20:21:26 +0100

    Updated CORS

--- IOC.EAssistant.Gateway/IOC.EAssistant.Gateway.Api/Extension/ApplicationBuilderExtension.cs
+++ IOC.EAssistant.Gateway/IOC.EAssistant.Gateway.Api/Extension/ApplicationBuilderExtension.cs
@@ -5,27 +5,47 @@ public static class ApplicationBuilderExtension
 {
     public static IApplicationBuilder UseCORSPolicy(this IApplicationBuilder app, IConfiguration conf)
     {
-        var def = conf.GetSection("CORS:__DefaultCorsPolicy").Get<CorsPolicy>();
+        var corsSection = conf.GetSection("CORS:__DefaultCorsPolicy");
+
+        if (!corsSection.Exists())
+        {
+            app.UseCors(builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            });
+            return app;
+        }
+
+        var def = corsSection.Get<CorsPolicy>();
 
         app.UseCors(builder =>
         {
-            if (def.Origins.Any())
+            if (def?.Origins != null && def.Origins.Any())
             {
-                if (def.Origins.Any(x => x.Contains('*'))) builder.SetIsOriginAllowedToAllowWildcardSubdomains();
-                builder.WithOrigins(def.Origins.ToArray());
+                if (def.Origins.Any(x => x == "*"))
+                {
+                    builder.AllowAnyOrigin();
+                }
+                else
+                {
+                    if (def.Origins.Any(x => x.Contains('*')))
+                        builder.SetIsOriginAllowedToAllowWildcardSubdomains();
+                    builder.WithOrigins(def.Origins.ToArray());
+                }
             }
+            else builder.AllowAnyOrigin();
 
-            if (def.Methods.Any()) builder.WithMethods(def.Methods.ToArray());
+            if (def?.Methods != null && def.Methods.Any()) builder.WithMethods(def.Methods.ToArray());
             else builder.AllowAnyMethod();
 
-            if (def.Headers.Any()) builder.WithHeaders(def.Headers.ToArray());
+            if (def?.Headers != null && def.Headers.Any()) builder.WithHeaders(def.Headers.ToArray());
             else builder.AllowAnyHeader();
 
-            if (def.ExposedHeaders.Any()) builder.WithExposedHeaders(def.ExposedHeaders.ToArray());
+            if (def?.ExposedHeaders != null && def.ExposedHeaders.Any()) builder.WithExposedHeaders(def.ExposedHeaders.ToArray());
 
-            if (def.SupportsCredentials) builder.AllowCredentials();
-
-            builder.Build();
+            if (def?.SupportsCredentials == true) builder.AllowCredentials();
         });
 
         return app;
